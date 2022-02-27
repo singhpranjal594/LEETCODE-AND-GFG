9be81f861d9048c0419130129490575272a3adc6
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-02-27 21:35:26 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (79.97%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int fib(int n) {
+        int dp[n+1];
+        if(n==0)
+            return 0;
+        dp[0]=0;
+        dp[1]=1;
+        // if(n==0)
+        //     return 0;
+        for(int i=2;i<=n;i++)
+        {
+            dp[i]=dp[i-1]+dp[i-2];
+            cout<<dp[i];
+        }
+        return dp[n];
+        
+    }
+};
