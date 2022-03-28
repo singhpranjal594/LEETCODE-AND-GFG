6d66b611c5926d97e25ba86715ad661cabe4ca9c
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-03-29 00:37:01 +0530

    Time: 11 ms (10.65%), Space: 7.7 MB (71.16%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -11,7 +11,8 @@ public:
         
         for(int i = 1; i <= m; i++) {
             for(int j = 1; j <= n; j++) {
-                if(!v[i - 1][j - 1]) dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                if(!v[i - 1][j - 1])
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
             }
         }
         
