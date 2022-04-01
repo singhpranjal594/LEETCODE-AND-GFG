32227da8ae6be333b96584c76cd8a7efe69d8fd8
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-04-01 23:23:00 +0530

    Time: 37 ms (31.15%), Space: 23.3 MB (41.72%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -7,9 +7,9 @@ public:
         char temp=' ';
         while(i<j)
         {
-             temp=s[j];
-            s[j]=s[i];
-            s[i]=temp;
+             temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
             i++;
             j--;
         }
