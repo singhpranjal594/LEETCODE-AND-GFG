0b5fecbae73a5dd43ff7c7e073f2fd765fc17b74
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-04-18 00:06:52 +0530

    Time: 0 ms (100.00%), Space: 8 MB (45.48%) - LeetHub

--- 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
+++ 897-increasing-order-search-tree/897-increasing-order-search-tree.cpp
@@ -1,31 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
 public:
-    TreeNode* cur=new TreeNode(0);
-    TreeNode* par=cur;
-    TreeNode* increasingBST(TreeNode* root) {
-        inorder(root);
-        return par->right;
+    
+    TreeNode* inorder(TreeNode *cur, TreeNode *ans) {
+        if (cur == nullptr) return ans;
+        TreeNode *now = inorder(cur->left, ans);
+        now->right = new TreeNode(cur->val);
+        return inorder(cur->right, now->right);
     }
-    void inorder(TreeNode* root)
-    {
-        if(root == NULL)
-            return;
-        
-        inorder(root->left);
-        TreeNode* p1=new TreeNode(root->val);
-        cur->right=p1;
-        cur=cur->right;
-        inorder(root->right);
+    
+    TreeNode* increasingBST(TreeNode* root) {
+        TreeNode *cur = new TreeNode();
+        inorder(root, cur);
+        return cur->right;
     }
 };
