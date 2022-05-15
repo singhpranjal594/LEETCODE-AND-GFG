2088842ac21629f0a5709defbb8eb2a9b89a9548
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-05-15 22:21:18 +0530

    Time: 236 ms (33.33%), Space: 57.6 MB (33.33%) - LeetHub

---
+++ 2275-largest-combination-with-bitwise-and-greater-than-zero/2275-largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int largestCombination(vector<int>& candidates) {
+        vector<int> v(33);
+        for(int i=0;i<candidates.size();i++)
+        {
+            int num=candidates[i];
+            int j=0;
+            while(num>0)
+            {
+                v[j]+=num%2;
+                num/=2;
+                j++;
+            }
+            
+        }
+        int maxi=INT_MIN;
+        for(int i=0;i<32;i++)
+        {
+            cout<<v[i];
+            maxi=max(maxi,v[i]);
+        }
+        return maxi;
+        
+        
+    }
+};
