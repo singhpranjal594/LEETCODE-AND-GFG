b35d1d357c49a7962cf0be672d3ad7a5e8ec818e
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-05-31 23:38:12 +0530

    Time: 152 ms (87.42%), Space: 63.2 MB (55.97%) - LeetHub

--- 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     bool hasAllCodes(string s, int k) {
-        set<string> se;
-        int n=s.size();
-        for(int i=0;i<=n-k;i++)
+        int i=0;
+        int j=0;
+        int n=pow(2,k);
+        vector<int> v(n,0);
+        int dec=0;
+        while(j<s.size())
         {
-            se.insert(s.substr(i,k));
+            dec=dec*2+(s[j]-'0');
+            if(j-i+1<k)
+            {
+                j++;
+            }
+            else if(j-i+1==k)
+            {
+                v[dec]=1;
+                dec-=(s[i]-'0')*pow(2,k-1);
+                i++;
+                j++;
+            }
         }
-        cout<<se.size();
-        if(se.size()==pow(2,k))
-            return true;
-        return false;
+        for(int i=0;i<n;i++)
+        {
+            if(!v[i])
+                return false;
+            
+        }
+        return true;
         
     }
 };
