d9c7a697fa77d713c2421c5ae96cd531aeac642e
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-07-14 00:01:25 +0530

    Time: 19 ms (69.54%), Space: 13.2 MB (52.88%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -3,30 +3,44 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m=matrix.size();
         int n=matrix[0].size();
-        set<int> s1;
-        set<int> s2;
         
+        bool flag=false;
         for(int i=0;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            if(matrix[i][0]==0)
+            {
+                flag=true;    
+            }
+            for(int j=1;j<n;j++)
             {
                 if(matrix[i][j]==0)
                 {
-                    s1.insert(i);
-                    s2.insert(j);     
+                    matrix[i][0]=0;
+                    matrix[0][j]=0;
                 }
             }
         }
-        for(int i=0;i<m;i++)
+        for(int i=1;i<m;i++)
         {
-            for(int j=0;j<n;j++)
+            for(int j=1;j<n;j++)
             {
-                if(s1.find(i)!=s1.end()||s2.find(j)!=s2.end())
-                {
+                if(matrix[i][0]==0||matrix[0][j]==0)
                     matrix[i][j]=0;
-                }
+                
             }
         }
-        
+        if(matrix[0][0]==0)
+        {
+            for(int i=0;i<n;i++)
+                matrix[0][i]=0;
+        }
+        if(flag)
+        {
+            for(int i=0;i<m;i++)
+            {
+                matrix[i][0]=0;
+                
+            }
+        }
     }
 };
