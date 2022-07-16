58ca665fbba13532dca325f36ad517172fb67e1b
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-07-16 13:14:26 +0530

    Time: 7 ms (89.25%), Space: 6.6 MB (71.79%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,59 +1,26 @@
-
 class Solution {
 public:
-    
-    // declare a dp table
-    
     int dp[55][55][55];
-    
-    long long mod = 1e9 + 7;
-    
-    int dfs(int i, int j, int n, int m, int moves)
+    long long  mod=1e9+7;
+    int f(int i,int j,int moves,int n,int m)
     {
-        // base case, if we reached out of grid
-        
-        if(i < 0 || i >= n || j < 0 || j >= m)
+        if(i<0||j<0||i>=n||j>=m)
             return 1;
-        
-        // if no moves are remaining
-        
-        if(moves <= 0)
+        if(moves<=0)
             return 0;
-        
-        // if already calculated
-        
-        if(dp[i][j][moves] != -1)
+        if(dp[i][j][moves]!=-1)
             return dp[i][j][moves];
-        
-        // call for upper side
-        
-        int up = dfs(i - 1, j, n, m, moves - 1);
-        
-        // call for down side
-        
-        int down = dfs(i + 1, j, n, m, moves - 1);
-        
-        // call for left side
-        
-        int left = dfs(i, j - 1, n, m, moves - 1);
-        
-        // call for right side
-        
-        int right = dfs(i, j + 1, n, m, moves - 1);
-        
-        // total paths will be sum of all the paths from all four directions
-        
-        // store the result and return it
-        
+        int up=f(i-1,j,moves-1,n,m);
+        int down=f(i+1,j,moves-1,n,m);
+        int left=f(i,j-1,moves-1,n,m);
+        int right=f(i,j+1,moves-1,n,m);
         return dp[i][j][moves] = (up % mod + down % mod + left % mod + right % mod) % mod;
+        
     }
-    
     int findPaths(int n, int m, int maxMove, int startRow, int startColumn) {
-        
-        // initialize the dp with -1
-        
         memset(dp, -1, sizeof(dp));
         
-        return dfs(startRow, startColumn, n, m, maxMove);
+        return f(startRow, startColumn, maxMove, n, m);
+        
     }
 };
