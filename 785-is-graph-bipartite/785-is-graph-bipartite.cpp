02d0c00c5614b722f4811c38a9c692f0325b8c46
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-08-11 01:32:49 +0530

    Time: 28 ms (85.50%), Space: 13.6 MB (60.91%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,35 +1,45 @@
-class Solution 
-{
-	public:
-	bool checkbfs(int node, vector<vector<int>> &graph, int color[])
-	{
-		if(color[node]==-1) color[node]=1;
-		for(auto it: graph[node])
-		{
-			if(color[it]==-1)
-			{
-				color[it]=1-color[node];
-				if(!checkbfs(it,graph,color)){
-					return false;
-				}
-			}
-			else if(color[it]==color[node]) return false;
-		}
-		return true;
-	}
-	bool isBipartite(vector<vector<int>>& graph) 
-	{
-		int n=graph.size();
-		int color[n];
-		memset(color,-1,sizeof color);
-		for(int i=0;i<n;i++){
-			if(color[i]==-1){
-				if(!checkbfs(i,graph,color)){
-					return false;
-				}
-			}
-		}
-		return true;
-
-	}
+class Solution {
+public:
+    bool co(int n,vector<int>&color,vector<vector<int>>& graph)
+    {
+        if(color[n]==-1)
+            color[n]=1;
+        for(auto it:graph[n])
+        {
+            if(color[it]==-1)
+            {
+                color[it]=1-color[n];
+                if(!co(it,color,graph))
+                    return false;
+            }
+            else if(color[it]==color[n])
+                return false;
+        }
+        return true;
+    }
+    bool isBipartite(vector<vector<int>>& graph) {
+        vector<int> color(graph.size(),-1);
+//         for(int i=0;i<graph.size();i++)
+//         {
+//             for(int j=0;j<graph[0].size();j++)
+//             {
+//                 cout<<graph[i][j];
+                
+//             }
+//             cout<<endl;
+//         }
+        for(int i=0;i<graph.size();i++)
+        {
+            if(color[i]==-1)
+            {
+            if(!co(i,color,graph))
+                return false;
+            }
+            
+            
+        }
+        return true;
+        
+        
+    }
 };
