6ceefb0a59d78a302e51cda1895ef4149b5e63cd
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-08-11 01:32:34 +0530

    Time: 27 ms (88.63%), Space: 13.5 MB (84.47%) - LeetHub

--- 785-is-graph-bipartite/785-is-graph-bipartite.cpp
+++ 785-is-graph-bipartite/785-is-graph-bipartite.cpp
@@ -1,42 +1,35 @@
-class Solution {
-public:
-    bool co(int n,vector<int>&color,vector<vector<int>>& graph)
-    {
-        queue<int> q;
-        q.push(n);
-        color[n]=1;
-        while(!q.empty())
-        {
-            int node=q.front();
-            q.pop();
-            for(int it:graph[node])
-            {
-                if(color[it]!=1)
-                {
-                    color[it]=1-color[node];
-                    q.push(it);
-                }
-                else if(color[it]==color[node])
-                    return false;
-                
-            }
-        }
-        return true;
-    }
-    bool isBipartite(vector<vector<int>>& graph) {
-        vector<int> color(graph.size(),-1);
-        for(int i=0;i<graph.size();i++)
-        {
-            if(color[i]==-1)
-            {
-            if(!co(i,color,graph))
-                return false;
-            }
-            
-            
-        }
-        return true;
-        
-        
-    }
+class Solution 
+{
+	public:
+	bool checkbfs(int node, vector<vector<int>> &graph, int color[])
+	{
+		if(color[node]==-1) color[node]=1;
+		for(auto it: graph[node])
+		{
+			if(color[it]==-1)
+			{
+				color[it]=1-color[node];
+				if(!checkbfs(it,graph,color)){
+					return false;
+				}
+			}
+			else if(color[it]==color[node]) return false;
+		}
+		return true;
+	}
+	bool isBipartite(vector<vector<int>>& graph) 
+	{
+		int n=graph.size();
+		int color[n];
+		memset(color,-1,sizeof color);
+		for(int i=0;i<n;i++){
+			if(color[i]==-1){
+				if(!checkbfs(i,graph,color)){
+					return false;
+				}
+			}
+		}
+		return true;
+
+	}
 };
