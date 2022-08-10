d98acac011466c3bef3fea11c3effe488a588b7b
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-08-11 02:15:39 +0530

    Time: 30 ms (67.62%), Space: 14.7 MB (23.34%) - LeetHub

--- 207-course-schedule/207-course-schedule.cpp
+++ 207-course-schedule/207-course-schedule.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    bool cycle(int node,vector<vector<int>>&adj,vector<int>&vis)
+    bool cycle(int node,vector<vector<int>>&adj,vector<int>&vis,vector<int>&dfs)
     {
-        if(vis[node]==2)
-            return true;
-        
-        vis[node]=2;
+        vis[node]=1;
+        dfs[node]=1;
         for(auto it:adj[node])
         {
-            if(vis[it]!=1)
+            if(vis[it]==0)
             {
-                if(cycle(it,adj,vis))
-                    return true;
+                if(cycle(it,adj,vis,dfs))return true;
             }
+            else if(dfs[it]==1)
+                    return true;
+            
         }
-        vis[node]=1;
+        dfs[node]=0;
         return false;
+        
     }
     bool canFinish(int n, vector<vector<int>>& p) {
         
         vector<int>vis(n,0);
+        vector<int> dfs(n,0);
         
         vector<vector<int>>adj(n);
         for(auto it:p)
@@ -29,11 +31,9 @@ public:
         
         for(int i=0;i<n;i++)
         {
-            if(vis[i]!=1){
-                if(cycle(i,adj,vis))
+            if(!vis[i] && cycle(i,adj,vis,dfs))
                     return false;
             }
-        }
         return true;
         
     }
