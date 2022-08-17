a44eda49f32a99ad936723ce62aa3184eb51df9e
Pranjal Singh <68381418+singhpranjal594@users.noreply.github.com>
2022-08-18 02:05:20 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (87.56%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    void f(vector<int> nums,vector<int> a,vector<vector<int>> &ans,int i)
-    {
-        if(i==nums.size())
-        {
-            ans.push_back(a);
-            return;
-        }
-        f(nums,a,ans,i+1);
-        a.push_back(nums[i]);
-        f(nums,a,ans,i+1);
-    }
-    
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> a;
-        f(nums,a,ans,0);
+        vector<vector<int>> ans{{}};
+        for(auto it:nums)
+        {
+            int si=ans.size();
+            for(int i=0;i<si;i++)
+            {
+                vector<int> f(ans[i]);
+                f.push_back(it);
+                ans.push_back(f);
+                
+            }
+        }
         return ans;
         
     }
